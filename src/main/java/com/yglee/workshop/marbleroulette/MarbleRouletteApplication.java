aee4ae4f4f2b35da5833c7435f12f74cfc4c65dd
yglee8048 <yglee8048@gmail.com>
2023-05-05 12:47:31 +0900

    project build

---
+++ src/main/java/com/yglee/workshop/marbleroulette/MarbleRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.yglee.workshop.marbleroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleRouletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yglee/workshop/marbleroulette/MarbleRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yglee.workshop.marbleroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarbleRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
