7efb581afe846d8969d6af003e92ffbe12656cdf
yglee8048 <yglee8048@gmail.com>
2023-05-18 13:06:03 +0900

    구슬 판

--- src/main/java/com/yglee/workshop/marbleroulette/api/MarbleApi.java
+++ src/main/java/com/yglee/workshop/marbleroulette/api/MarbleApi.java
@@ -3,11 +3,13 @@ package com.yglee.workshop.marbleroulette.api;
 import com.yglee.workshop.marbleroulette.model.MemberDTO;
 import com.yglee.workshop.marbleroulette.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -18,21 +20,21 @@ public class MarbleApi {
     private final MemberService memberService;
 
     @GetMapping
-    public String getScoredList(@RequestParam(value = "excludes", required = false) String excludes) {
+    public List<String> getScoredList(@RequestParam(value = "excludes", required = false) List<String> excludes) {
         return memberService.getMemberRankings()
                 .stream()
-                .filter(memberRanking -> excludes == null || !excludes.contains(memberRanking.getId()))
+                .filter(memberRanking -> CollectionUtils.isEmpty(excludes) || !excludes.contains(memberRanking.getId()))
                 .filter(memberRanking -> memberRanking.getTotalScore() > 0)
                 .map(memberRanking ->
-                        String.format("%s * %d", memberRanking.getName(), memberRanking.getTotalScore() / 10))
-                .collect(Collectors.joining(", "));
+                        String.format("%s * %d", memberRanking.getName(), memberRanking.getTotalScore()))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/no-score")
-    public String getNonScoredList() {
+    public List<String> getNonScoredList() {
         return memberService.getAllMembers()
                 .stream()
                 .map(MemberDTO::getName)
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.toList());
     }
 }
