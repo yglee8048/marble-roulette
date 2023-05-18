61fbb7fef2b4f84435f2f97b1c539a26ec99fc54
yglee8048 <yglee8048@gmail.com>
2023-05-18 13:47:05 +0900

    change ps grep

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(ps -ef | grep marble-roulette | grep jar | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep marble-roulette | grep jar | awk '{print $2}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
